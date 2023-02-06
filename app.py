bd88fb223a5e1ce547f5e644f26a744c2d7903e0
leafsheep <sheep29@sina.com>
2023-02-07 01:46:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home_endpoint():
     return 'Hello World!'
 
 
-@app.route('/predict',methods=['GET', 'POST'])
+@app.route('/predict',methods=['POST'])
 def get_prediction():
     # Works only for a single sample
     if request.method == 'POST':
